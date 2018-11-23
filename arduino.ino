9df6910b964ef0edd46e82adce09a794e523c4e7
Mateus Fontoura <fontourasantana@gmail.com>
2018-11-23 02:00:57 -0200

    Código do arduino refatorado

--- arduino.ino
+++ arduino.ino
@@ -1,17 +1,24 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <IRremote.h>
+#define RECV_PIN 2
+#define CODE_CHECKIN 1
+#define CODE_CHECKOUT 0
+#define SYSTEM_STATUS 0
+#define SYSTEM_USER_IN_USE 1
+//#include <LiquidCrystal.h>
 
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+// LCD
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+/////
+byte MAC[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+EthernetClient client;
 IPAddress ip(10, 0, 0, 103);
 IPAddress gateway(10, 0, 0, 1);
 char server[] = "10.0.0.100";
-EthernetClient client;
-int contador = 0, statusCount = 0, aux = 0, auxCount = 0;
-char statusCode[4] = "";
-char bufferAux[255] = "";
-char CODE[3], TOKEN[40];
+///
+//char CODE[3], TOKEN[40];
 
 // CIMA - FF629D
 // DIREITA - FFC23D
@@ -33,49 +40,77 @@ char CODE[3], TOKEN[40];
 
 // IR
 int CODE_NUMBER_ONE = 5, CODE_NUMBER_TWO = 6, CODE_NUMBER_THREE = 7;
-int MAPPED_CONTROL_CODES[17] = {0xFF629D, // CIMA
-                                0xFFC23D, // DIREITA
-                                0xFFA857, // BAIXO
-                                0xFF22DD, // ESQUERDA
-                                0xFF02FD, // OK
-                                0xFF6897, // 1
-                                0xFF9867, // 2
-                                0xFFB04F, // 3
-                                0xFF30CF, // 4
-                                0xFF18E7, // 5
-                                0xFF7A85, // 6
-                                0xFF10EF, // 7
-                                0xFF38C7, // 8
-                                0xFF5AA5, // 9
-                                0xFF4AB5, // 0
-                                0xFF42BD, // *
-                                0xFF52AD // #
-                                };
-int RECV_PIN = 2;
-IRrecv irrecv(RECV_PIN);  
+int MAPPED_CONTROL_CODES[17] = {
+    0xFF629D, // CIMA
+    0xFFC23D, // DIREITA
+    0xFFA857, // BAIXO
+    0xFF22DD, // ESQUERDA
+    0xFF02FD, // OK
+    0xFF6897, // 1
+    0xFF9867, // 2
+    0xFFB04F, // 3
+    0xFF30CF, // 4
+    0xFF18E7, // 5
+    0xFF7A85, // 6
+    0xFF10EF, // 7
+    0xFF38C7, // 8
+    0xFF5AA5, // 9
+    0xFF4AB5, // 0
+    0xFF42BD, // *
+    0xFF52AD  // #
+};
+
+IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-int flag=0, SYSTEM_INFO[2] = {0, 0}, TEMP_CODE_READ=0;
-void setup() {
+// CONFIGURAÇÕES
+String returnOfRequisition, USER_LOGGED_CODE, USER_LOGGED_TOKEN, TEMP_USER_CODE, TEMP_USER_TOKEN;
+bool requestConfirmed = false;
+int SYSTEM_INFORMATION[2] = {0, 0}, TEMP_CODE_READ = 0;
+
+void setup()
+{
+    //    lcd.begin(16, 2);
     Serial.begin(9600);
     irrecv.enableIRIn();
     pinMode(RECV_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(RECV_PIN), function, RISING);
-    while (!Serial);
-    if (Ethernet.begin(mac) == 0) {
+    attachInterrupt(digitalPinToInterrupt(RECV_PIN), inputTreatmentIR, RISING);
+    while (!Serial)
+        ;
+    if (Ethernet.begin(MAC) == 0)
+    {
         Serial.println("Falha ao configurar Ethernet usando o DHCP");
-        Ethernet.begin(mac, ip);
-    } else
-        Serial.println("Iniciado");
+        Ethernet.begin(MAC, ip);
+    }
+    else
+        Serial.println("SISTEMA INICIALIZADO COM SUCESSO !");
 }
 
-void post(int METHOD) {
-    Serial.println("connecting...");
-    String postData = String("code=" + String(CODE) + "&security_token=" + String(TOKEN));
-    //Serial.println(postData);
-    if (client.connect(server, 8080)) {
-        Serial.println("connected");
-        if(METHOD == 1) client.println("POST /api/check_in/ HTTP/1.1"); else client.println("POST /api/check_out/ HTTP/1.1");
+void requestHttp(String CODE, String TOKEN, int METHOD_REQUEST)
+{
+    if (METHOD_REQUEST == CODE_CHECKIN)
+        Serial.println("Verificando Identificação");
+    else
+        Serial.println("Registrando Saída");
+
+    // lcd.clear();
+    // lcd.setCursor(0, 0);
+    // lcd.print("VERIFICANDO");
+    // lcd.setCursor(0, 1);
+    // lcd.print("IDENTIFICACAO");
+
+    // lcd.clear();
+    // lcd.setCursor(0, 0);
+    // lcd.print("REGISTRANDO");
+    // lcd.setCursor(0, 1);
+    // lcd.print("SAIDA");
+    String postData = String("code=" + CODE + "&security_token=" + TOKEN);
+    if (client.connect(server, 8080))
+    {
+        if (METHOD_REQUEST == CODE_CHECKIN)
+            client.println("POST /api/check_in/ HTTP/1.1");
+        else
+            client.println("POST /api/check_out/ HTTP/1.1");
         client.println("User-Agent: Arduino/1.0");
         client.println("Connection: close");
         client.println("Content-Type: application/x-www-form-urlencoded;");
@@ -83,82 +118,137 @@ void post(int METHOD) {
         client.println(postData.length());
         client.println();
         client.println(postData);
-    } else {
-        Serial.println("connection failed");
+        requestConfirmed = true;
     }
-    flag=1;
-    contador = 0; statusCount = 0; aux = 0; auxCount = 0;
+    else
+    {
+        Serial.println("Erro na verificação da identificação");
+        // lcd.clear();
+        // lcd.setCursor(0, 0);
+        // lcd.print("OCORREU UM ERRO");
+        // lcd.setCursor(0, 1);
+        // lcd.print("TENTE NOVAMENTE");
+    }
 }
 
-void function() {
-  if (irrecv.decode(&results)){
-        //Serial.print("Valor lido : ");  
-        //Serial.println(results.value, HEX);
+void inputTreatmentIR()
+{
+    if (irrecv.decode(&results))
+    {
+        noInterrupts();
         int CODE_READ = (results.value);
+        String CODE, TOKEN;
         Serial.println(CODE_READ, HEX);
-        if(SYSTEM_INFO[0] == 0){
-            if (CODE_READ == MAPPED_CONTROL_CODES[CODE_NUMBER_ONE]){
-              Serial.println("Tecla 1 Pressionada");
-              strcpy(CODE, "123");
-              strcpy(TOKEN, "918237m12387da6sd876xcz765123*!SDSxasd1");
-              post(1);
+        if (SYSTEM_INFORMATION[SYSTEM_STATUS] == 0 && CODE_READ != 0xFFFFFFFF)
+        {
+            if (CODE_READ == MAPPED_CONTROL_CODES[CODE_NUMBER_ONE])
+            {
+                CODE = "123";
+                TOKEN = "918237m12387da6sd876xcz765123*!SDSxasd1";
             }
             if (CODE_READ == MAPPED_CONTROL_CODES[CODE_NUMBER_TWO])
-            {  
-              Serial.println("Tecla 2 Pressionada");
-              strcpy(CODE, "456");
-              strcpy(TOKEN, "918237m12387da6sd876xcz765123*!SDSxasd2");
-              post(1);
-            }  
+            {
+                CODE = "456";
+                TOKEN = "918237m12387da6sd876xcz765123*!SDSxasd2";
+            }
             if (CODE_READ == MAPPED_CONTROL_CODES[CODE_NUMBER_THREE])
             {
-              Serial.println("Tecla 3 Pressionada");
-              strcpy(CODE, "789");
-              strcpy(TOKEN, "918237m12387da6sd876xcz765123*!SDSxasd3");
-              post(1);
+                CODE = "789";
+                TOKEN = "918237m12387da6sd876xcz765123*!SDSxasd3";
             }
             TEMP_CODE_READ = CODE_READ;
-        } else {
-            Serial.print("Sistema em uso, pelo código: ");
-            Serial.println(SYSTEM_INFO[1], HEX);
-            if (CODE_READ == SYSTEM_INFO[1]){
-                strcpy(CODE, "123");
-                strcpy(TOKEN, "918237m12387da6sd876xcz765123*!SDSxasd1");
-                post(0);
+            TEMP_USER_CODE = CODE;
+            TEMP_USER_TOKEN = TOKEN;
+            requestHttp(CODE, TOKEN, CODE_CHECKIN);
+        }
+        else if (SYSTEM_INFORMATION[SYSTEM_STATUS] == 1)
+        {
+            Serial.print("Sistema em uso: ");
+            Serial.println(String(USER_LOGGED_CODE + " - " + USER_LOGGED_TOKEN));
+            if (CODE_READ == SYSTEM_INFORMATION[SYSTEM_USER_IN_USE])
+            {
+                CODE = USER_LOGGED_CODE;
+                TOKEN = USER_LOGGED_TOKEN;
+                requestHttp(CODE, TOKEN, CODE_CHECKOUT);
             }
         }
-        irrecv.resume(); //Le o próximo valor
-  }
+        irrecv.resume();
+        interrupts();
+    }
 }
 
-void loop(){
-    if(flag){
-        if (client.available()) {
-            contador++;
-            char c = client.read();
-            if(contador >= 10 && contador <= 12) statusCode[statusCount++] = c;
-            if(aux == 7) bufferAux[auxCount++] = c;
-            if(c == '\n') aux++;
-        }
-        if (!client.connected()) {
-            Serial.println();
-            Serial.println("Desconectando.");
+void loop()
+{
+    if (requestConfirmed)
+    {
+        if (client.available())
+            returnOfRequisition.concat((char)client.read());
+        if (!client.connected())
+        {
             client.stop();
-            statusCode[statusCount] = '\0';
-            bufferAux[0] = ' ';
-            bufferAux[auxCount-1] = '\0';
-            Serial.println(statusCode);
-            if(strcmp("200", statusCode) == 0){
-                if(strlen(bufferAux) > 1){ // Retornou nome do professor
-                    Serial.println(String("Bem vindo" + String(bufferAux)));
-                    SYSTEM_INFO[0] = 1;
-                    SYSTEM_INFO[1] = TEMP_CODE_READ;
-                } else { // Retornou nada, entretanto como código 200 logo registrou a saída do professor
-                    SYSTEM_INFO[0] = 0;
-                    SYSTEM_INFO[1] = 0;
+            long statusCode = getStatusCode(returnOfRequisition);
+            String username = getUsername(returnOfRequisition);
+            if (statusCode == 200)
+            {
+                if (username.length() > 0)
+                { // Retornou nome do professor
+                    Serial.println(String("Bem vindo " + username));
+                    // lcd.clear();
+                    // lcd.setCursor(0, 0);
+                    // lcd.print("BEM VINDO");
+                    // lcd.setCursor(0, 1);
+                    // lcd.print(username);
+                    SYSTEM_INFORMATION[SYSTEM_STATUS] = 1;
+                    SYSTEM_INFORMATION[SYSTEM_USER_IN_USE] = TEMP_CODE_READ;
+                    USER_LOGGED_CODE = TEMP_USER_CODE;
+                    USER_LOGGED_TOKEN = TEMP_USER_TOKEN;
+                }
+                else
+                { // Retornou nada, entretanto como código 200 logo registrou a saída do professor
+                    // lcd.clear();
+                    // lcd.setCursor(0, 0);
+                    // lcd.print("SAIDA REGISTRADA");
+                    // lcd.setCursor(0, 1);
+                    // lcd.print("COM SUCESSO");
+                    Serial.println("Saída Registrada Com Sucesso");
+                    SYSTEM_INFORMATION[SYSTEM_STATUS] = 0;
+                    SYSTEM_INFORMATION[SYSTEM_USER_IN_USE] = 0;
+                    USER_LOGGED_CODE = "";
+                    USER_LOGGED_TOKEN = "";
                 }
             }
-            flag=0;
+            returnOfRequisition = "";
+            requestConfirmed = false;
         }
     }
+}
+
+long getStatusCode(String str)
+{
+    String statusCode = str.substring(9, 12);
+    return statusCode.toInt();
+}
+
+String removeUnnecessaryInformation(String response)
+{
+    int pos = response.lastIndexOf('\n') + 1, stringSize = response.length();
+    return response.substring(pos, stringSize);
+}
+
+bool isReturnOfCheckoutRequest(String response)
+{
+    return (response.equals('0')) ? true : false;
+}
+
+String getUsernameFromInformation(String response)
+{
+    String username = response.substring(1, response.length() - 1);
+    username.toUpperCase();
+    return username;
+}
+
+String getUsername(String response)
+{
+    String informationReturned = removeUnnecessaryInformation(response);
+    return (isReturnOfCheckoutRequest(informationReturned)) ? "" : getUsernameFromInformation(informationReturned);
 }
